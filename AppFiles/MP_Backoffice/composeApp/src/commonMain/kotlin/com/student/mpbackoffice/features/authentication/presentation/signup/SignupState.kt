946c20b6d9fbeeb9e89bed79bad0cdf45a5512a6
João Oliveira <1191457@isep.ipp.pt>
2025-05-27 02:59:44 +0100

    Fixes in Signup feature in backoffice app. The app now builds once again

--- AppFiles/MP_Backoffice/composeApp/src/commonMain/kotlin/com/student/mpbackoffice/features/authentication/presentation/signup/SignupState.kt
+++ AppFiles/MP_Backoffice/composeApp/src/commonMain/kotlin/com/student/mpbackoffice/features/authentication/presentation/signup/SignupState.kt
@@ -3,8 +3,9 @@ package com.student.mpbackoffice.features.authentication.presentation.signup
 import com.student.mpbackoffice.core.presentation.UiText
 
 data class SignupState(
-    val email: String = "",
+    val email: String = "Email",
     val password: String = "",
     val isLoading: Boolean = false,
+    val loginSuccessful: Boolean = false,
     val errorMessage: UiText? = null
 )
--- AppFiles/MP_Backoffice/composeApp/src/commonMain/kotlin/com/student/mpbackoffice/features/authentication/presentation/signup/SignupViewModel.kt
+++ AppFiles/MP_Backoffice/composeApp/src/commonMain/kotlin/com/student/mpbackoffice/features/authentication/presentation/signup/SignupViewModel.kt
@@ -4,8 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.student.mpbackoffice.core.presentation.UiText
 import com.student.mpbackoffice.features.authentication.domain.repository.AuthRepository
+import com.student.mpbackoffice.core.domain.onError
+import com.student.mpbackoffice.core.domain.onSuccess
+import com.student.mpbackoffice.core.presentation.toUiText
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class SignupViewModel(
@@ -26,25 +30,38 @@ class SignupViewModel(
             is SignupAction.OnSignupClick -> {
                 signup(action.email, action.password)
             }
+            is SignupAction.OnBackToLoginClick -> TODO()
         }
     }
 
     private fun signup(email: String, password: String) {
         viewModelScope.launch {
-            _state.value = _state.value.copy(isLoading = true, errorMessage = null)
-
-            val result = authRepository.signup(email, password)
-
-            _state.value = if (result.isSuccess) {
-                _state.value.copy(isLoading = false)
-            } else {
-                _state.value.copy(
-                    isLoading = false,
-                    errorMessage = UiText.DynamicString(
-                        result.exceptionOrNull()?.message ?: "Unknown error"
-                    )
+            _state.update {
+                it.copy(
+                    isLoading = true, errorMessage = null
                 )
             }
+
+            authRepository
+                .signup(_state.value.email, _state.value.password)
+                .onSuccess {
+                    _state.update {
+                        it.copy(
+                            isLoading = true,
+                            errorMessage = null,
+                            loginSuccessful = true
+                        )
+                    }
+                }
+                .onError { error ->
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            errorMessage = error.toUiText(),
+                            loginSuccessful = false
+                        )
+                    }
+                }
         }
     }
 }
