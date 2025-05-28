12c94c7030bfbb657869a6534d543e4905471209
João Oliveira <1191457@isep.ipp.pt>
2025-05-28 12:01:38 +0100

    Removed old backoffice app files

--- AppFiles/old_files/MP_BackofficeApp/composeApp/src/commonMain/kotlin/com/student/mpbackoffice/features/authentication/domain/model/Admin.kt
+++
@@ -1,7 +0,0 @@
-package com.student.mpbackoffice.features.authentication.domain.model
-
-data class Admin(
-    val uid: String,
-    val email: String,
-    val token: String // Auth token or session ID returned by server
-)
--- AppFiles/old_files/MP_BackofficeApp/shared/src/commonMain/kotlin/com/student/mpbackoffice/Constants.kt
+++
@@ -1,3 +0,0 @@
-package com.student.mpbackoffice
-
-const val SERVER_PORT = 8080
